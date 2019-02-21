1c3b3eabc9e30c7e1ff715f16041bf5aa7d5d40c
Ibrahim mitou <mitou70@gmail.com>
2019-02-21 20:30:10 +0100

    Add an optimized implementation of boxing items + tests

--- src/main/java/com/ibrahim/xspeedit/RobotBoxer.java
+++ src/main/java/com/ibrahim/xspeedit/RobotBoxer.java
@@ -1,6 +1,11 @@
 package com.ibrahim.xspeedit;
 
-import java.util.*;
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -8,26 +13,67 @@ import static org.apache.logging.log4j.util.Strings.isBlank;
 
 public class RobotBoxer {
 
+    public static final String ITEM_SEPARATOR = "/";
+
     public static String boxItems(String itemsAsString) {
         List<Integer> itemsToBox = convertStringToListOfIntegers(itemsAsString);
-        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder boxedItems = new StringBuilder();
         int counter = 0;
         for (Integer currentItem : itemsToBox) {
             if (currentItem + counter <= 10) {
-                stringBuilder.append(currentItem);
+                boxedItems.append(currentItem);
                 counter += currentItem;
             } else {
-                stringBuilder.append("/").append(currentItem);
+                boxedItems.append(ITEM_SEPARATOR).append(currentItem);
                 counter = currentItem;
             }
         }
-        String boxedItems = stringBuilder.toString();
-        return boxedItems;
+        String boxedItemsAsAString = boxedItems.toString();
+        return boxedItemsAsAString;
+    }
+
+    /**
+     * This is one possible implementation of an optimized boxing
+     *  steps
+     *      step 1 : split the list of items into two lists (the first one from one to 5 and the second one from 6 to 9
+     *      step 2 : merge the two lists and alternate to elements on order to put the higher element close to small ones
+     *      step 3 : apply the simple boxItems algorithm
+     * @return String of boxed items optimally
+     */
+
+    public static String convertToBoxesOptimal(String itemsAsString) {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Integer> itemsAsList = convertStringToListOfIntegers(itemsAsString);
+        Collection<List<Integer>> SplittedListToBox = itemsAsList.stream().sorted().collect(Collectors.partitioningBy(o -> o > 5)).values();
+
+        if (SplittedListToBox.size() == 2) {
+            List<Integer> lowElements = new ArrayList<>(SplittedListToBox).get(0);
+            List<Integer> highElements = new ArrayList<>(SplittedListToBox).get(1);
+            Collections.reverse(highElements);
+            List<Integer> mergedList = mergeAlternatingElements(lowElements, highElements);
+            mergedList.forEach(stringBuilder::append);
+            String boxedItems = boxItems(stringBuilder.toString());
+            return boxedItems;
+        } else {
+            return Strings.EMPTY;
+        }
+    }
+
+    protected static List<Integer> mergeAlternatingElements(List<Integer> a, List<Integer> b) {
+        List<Integer> result = new ArrayList<>();
+        int size = Math.max(a.size(), b.size());
+
+        for (int i = 0; i < size; i++) {
+            if (i < a.size()) result.add(a.get(i));
+            if (i < b.size()) result.add(b.get(i));
+        }
+        return result;
     }
 
     protected static List<Integer> convertStringToListOfIntegers(String itemsAsString) {
-        if(isBlank(itemsAsString))
+        if (isBlank(itemsAsString))
             return Collections.emptyList();
+
         List<Integer> itemsAsIntegers = Stream.of(itemsAsString.split("")).map(Integer::parseInt).collect(Collectors.toList());
         return itemsAsIntegers;
     }
--- src/test/java/com/ibrahim/xspeedit/RobotBoxerTest.java
+++ src/test/java/com/ibrahim/xspeedit/RobotBoxerTest.java
@@ -3,9 +3,6 @@ package com.ibrahim.xspeedit;
 import org.apache.logging.log4j.util.Strings;
 import org.junit.Test;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -14,14 +11,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RobotBoxerTest {
 
     public static final String GIVEN_ITEMS = "163841689525773";
+    public static final String GIVEN_ITEMS2 = "111756329124581597532486263841689525773";
     public static final String EXPECTED_UNOPTIMIZED_ITEMS = "163/8/41/6/8/9/52/5/7/73";
+    public static final String EXPECTED_UNOPTIMIZED_ITEMS2 = "1117/5/63/2/91/24/5/81/5/9/7/532/4/8/62/63/8/41/6/8/9/52/5/7/73";
+    public static final String EXPECTED_OPTIMIZED_ITEMS = "19/18/28/37/37/46/5/6/5";
+    public static final String EXPECTED_OPTIMIZED_ITEMS2 = "19/19/19/181/81/82/82/72/72/72/73/63/63/63/64/44/55/55/55";
+
 
     @Test
-    public void should_return_wraped_boxes() {
+    public void should_return_wrapped_boxes() {
         //WHEN
         String boxedItems = RobotBoxer.boxItems(GIVEN_ITEMS);
         //THEN
         assertThat(boxedItems).isEqualTo(EXPECTED_UNOPTIMIZED_ITEMS);
+    }
+
+    @Test
+    public void should_return_wrapped_boxes2() {
+        //WHEN
+        String boxedItems = RobotBoxer.boxItems(GIVEN_ITEMS2);
+        //THEN
+        assertThat(boxedItems).isEqualTo(EXPECTED_UNOPTIMIZED_ITEMS2);
     }
 
     @Test
@@ -36,6 +46,38 @@ public class RobotBoxerTest {
     public void should_return_an_empty_list_when_items_are_null() {
         //WHEN
         String boxedItems = RobotBoxer.boxItems(null);
+        //THEN
+        assertThat(boxedItems).isEqualTo(Strings.EMPTY);
+    }
+
+    @Test
+    public void should_return_wrapped_boxes_optimal() {
+        //WHEN
+        String boxedItems = RobotBoxer.convertToBoxesOptimal(GIVEN_ITEMS);
+        //THEN
+        assertThat(boxedItems).isEqualTo(EXPECTED_OPTIMIZED_ITEMS);
+    }
+
+    @Test
+    public void should_return_wrapped_boxes_optimal2() {
+        //WHEN
+        String boxedItems = RobotBoxer.convertToBoxesOptimal(GIVEN_ITEMS2);
+        //THEN
+        assertThat(boxedItems).isEqualTo(EXPECTED_OPTIMIZED_ITEMS2);
+    }
+
+    @Test
+    public void should_return_an_empty_list_when_empty_string_and_calling_optimal_boxing() {
+        //WHEN
+        String boxedItems = RobotBoxer.convertToBoxesOptimal(Strings.EMPTY);
+        //THEN
+        assertThat(boxedItems).isEqualTo(Strings.EMPTY);
+    }
+
+    @Test
+    public void should_return_an_empty_list_when_items_are_null_and_calling_optimal_boxing() {
+        //WHEN
+        String boxedItems = RobotBoxer.convertToBoxesOptimal(null);
         //THEN
         assertThat(boxedItems).isEqualTo(Strings.EMPTY);
     }
